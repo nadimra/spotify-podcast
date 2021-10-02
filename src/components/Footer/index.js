10b26bd415068f1048cf1533bb236e399d5adeb9
nadimra <nadimrahman98@gmail.com>
2021-10-02 01:31:19 +0100

    website

---
+++ src/components/Footer/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle,FooterLink } from './FooterElements'
+import {SocialMedia, SocialMediaWrap,SocialLogo,WebsiteRights,SocialIcons, SocialIconLink} from './FooterElements'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+
+                    <FooterLinksWrapper>
+                        
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla © {new Date().getFullYear()} 
+                        All rights reserved.</WebsiteRights>
+
+                        <SocialIcons>
+                            <SocialIconLink href="//www.facebook.com" target="_blank" aria-label="Facebook">
+                                <FaFacebook/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                                <FaInstagram/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="YouTube">
+                                <FaYoutube/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                                <FaTwitter/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="LinkedIn">
+                                <FaLinkedin/>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+
+    )
+}
+
+export default Footer
