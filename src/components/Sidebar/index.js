68dc7cff6cb95bb1cdd788499e1014862d09d95b
nadimra <nadimrahman98@gmail.com>
2021-12-02 20:00:13 +0000

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -10,8 +10,13 @@ const Sidebar = ({isOpen, toggle}) => {
 
             <SidebarWrapper>
                 <SidebarMenu>
+                    <SidebarLink to ="speaker" onClick={toggle}>Intro</SidebarLink>
+                    <SidebarLink to ="reason1" onClick={toggle}>Reason #1</SidebarLink>
+                    <SidebarLink to ="reason2" onClick={toggle}>Reason #2</SidebarLink>
+                    <SidebarLink to ="reason3" onClick={toggle}>Reason #3</SidebarLink>
+                    <SidebarLink to ="reason4" onClick={toggle}>Reason #4</SidebarLink>
+                    <SidebarLink to ="reason5" onClick={toggle}>Reason #5</SidebarLink>
                     <SidebarLink to ="projects" onClick={toggle}>Projects</SidebarLink>
-                    <SidebarLink to ="contact" onClick={toggle}>Contact</SidebarLink>
                 </SidebarMenu>
 
             </SidebarWrapper>
